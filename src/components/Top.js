b474208453e43aa8ddc24c17048543214ef2eb3d
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-03 10:35:00 -0300

    centered top-header content

--- src/components/Top.js
+++ src/components/Top.js
@@ -13,11 +13,12 @@ const Top = () => {
         </div>
         <div className='top-body'>
           <div className='top-left-side'>
-            <p className='tls-first'>Conquiste a sua tão sonhada liberdade.</p>
-            <p className='tls-second'>Crie fontes que trabalharão para você enquanto viaja o mundo ou simplesmente curte sua familia em casa.</p>
+            Conquiste a sua tão sonhada liberdade.
+            <em>Crie fontes que trabalharão para você enquanto viaja o mundo ou simplesmente curte sua familia em casa.</em>
+            <button className='btn-top'>Quero ser nômade digital</button>
           </div>
           <div className='top-right-side'>
-            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="740" height="443" type="text/html" src="https://www.youtube.com/embed/_ysd-zHamjk?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=http://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/de/">youtubeembedcode.com/de/</a></small></div><div><small><a href="https://unoregler.com/">regler för uno</a></small></div></iframe>
+            <iframe className='iframe' frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="618" height="347" type="text/html" src="https://www.youtube.com/embed/_ysd-zHamjk?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=http://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/de/">youtubeembedcode.com/de/</a></small></div><div><small><a href="https://unoregler.com/">regler för uno</a></small></div></iframe>
           </div>
         </div>
       </div>
