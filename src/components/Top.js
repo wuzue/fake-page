1a670ae39a7906e5801ab79a362ca9d50448b0c1
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-03 09:04:25 -0300

    create header and main content structure

---
+++ src/components/Top.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Top.css'
+import imgOne from '../assets/imgs/imgone.jpg'
+import planeSvg from '../assets/imgs/plane-svg.svg'
+
+const Top = () => {
+  return (
+    <div className='top-main-div'>
+      <div className='top-container'>
+        <div className='top-logo'>
+          <img className='svg-logo' src={planeSvg} alt='.'/>
+          <p className='logo-text'>FakePage<br></br><span className='digital-text'>Digital</span></p>
+        </div>
+        <div className='top-body'>
+          <div className='top-left-side'>
+            <p className='tls-first'>Conquiste a sua tão sonhada liberdade.</p>
+            <p className='tls-second'>Crie fontes que trabalharão para você enquanto viaja o mundo ou simplesmente curte sua familia em casa.</p>
+          </div>
+          <div className='top-right-side'>
+            <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="740" height="443" type="text/html" src="https://www.youtube.com/embed/_ysd-zHamjk?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=http://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/de/">youtubeembedcode.com/de/</a></small></div><div><small><a href="https://unoregler.com/">regler för uno</a></small></div></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Top
