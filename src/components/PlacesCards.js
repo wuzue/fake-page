f7a3c53b56e2c04377fd423ff0ee64127ec698c3
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-03 14:15:49 -0300

    places cards are now responsive

---
+++ src/components/PlacesCards.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './PlacesCards.css'
+import paris from '../assets/imgs/paris.png'
+import moscow from '../assets/imgs/moscow.png'
+import losAngeles from '../assets/imgs/losangeles.jpg'
+
+const PlacesCards = () => {
+  return (
+    <div class="container">
+
+<div class="travel-card"><img src={paris} loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 28vw, (max-width: 991px) 30vw, 300px" alt="" class="image"/>
+<div class="travel-card-footer">
+  <p class="paragraph-2">Paris, França</p>
+</div>
+</div>
+
+<div class="travel-card"><img src={moscow} loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 28vw, (max-width: 991px) 30vw, 300px" alt="" class="image"/>
+<div class="travel-card-footer">
+  <p class="paragraph-2">Moscow, Rússia</p>
+</div>
+
+</div>
+
+<div class="travel-card"><img src={losAngeles} loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 28vw, (max-width: 991px) 30vw, 300px" alt="" class="image"/>
+<div class="travel-card-footer">
+  <p class="paragraph-2">Los Angeles, EUA</p>
+</div>
+</div>
+
+</div>
+  )
+}
+
+export default PlacesCards
--- src/components/SecondSec.js
+++ src/components/SecondSec.js
@@ -1,32 +1,14 @@
 import React from 'react'
+import PlacesCards from './PlacesCards'
 import './SecondSec.css'
-import paris from '../assets/imgs/paris.png'
-import moscow from '../assets/imgs/moscow.png'
-import losAngeles from '../assets/imgs/losangeles.jpg'
+
 
 const SecondSec = () => {
   return (
     <div className='second-main-div'>
       <p id='textone-second-sec'>Para onde você gostaria de viajar?</p>
       <div className='second-section-card'>
-        <div className='card'>
-          <img src={paris} alt='..'/>
-          <div className='text'>
-            <p>Paris, França</p>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={moscow} alt='..'/>
-          <div className='text'>
-            <p>Moscou, Russia</p>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={losAngeles} alt='..'/>
-          <div className='text'>
-            <p>Los Angeles, EUA</p>
-          </div>
-        </div>
+        <PlacesCards/>
       </div>
     </div> 
   )
