9284b058e8391f61a0e4119cb9a838d2eb017181
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-04 16:10:24 -0300

    dinamic card prototype

--- src/components/SecondSec.js
+++ src/components/SecondSec.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlacesCards from './PlacesCards'
 import './SecondSec.css'
+import DinamicCards from './DinamicCards';
+
 
 
 const SecondSec = () => {
-  return (
+  return(
     <div className='second-main-div'>
-      <p id='textone-second-sec'>Para onde você gostaria de viajar?</p>
+      <p className='textone-second-sec'>Para onde você gostaria de viajar?</p>
+      <a href='#img-paris'>Click me</a>
       <div className='second-section-card'>
         <PlacesCards/>
+      </div>
+      <div className='post-cards'>
+        <p className='title-post-cards'>Então conheça o Nômade Digital</p>
+        <p className='p-post-cards'>O Nômade Digital é o treinamento completo e o <em>passo a passo</em> para você criar
+        receitas passivas e ativas, conquistando as tão sonhadas:</p>
+      </div>
+      <div className='dinamic-part'>
+        <DinamicCards/>
       </div>
     </div> 
   )
 }
 
-export default SecondSec
+export default SecondSec;
