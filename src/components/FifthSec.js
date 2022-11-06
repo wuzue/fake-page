7febec07f5f1da0cd69db6a4c57d5875d1cdbb17
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-06 16:00:52 -0300

    started bottom of fifth part

--- src/components/FifthSec.js
+++ src/components/FifthSec.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './FifthSec.css'
 import travel from '../assets/imgs/travel.png'
+import planeSvg from '../assets/imgs/plane-svg.svg'
+
 
 const FifthSec = () => {
   return (
@@ -14,6 +16,28 @@ const FifthSec = () => {
         </div>
         <div className='fifth-right-side'>
           <img className='fifth-img' src={travel} alt='aaa'/>
+        </div>
+      </div>
+      <div className='fifth-container-two'>
+        <div className='fifth-mid'>
+          <div className='logo-fifth'>
+            <img className='svg-logo-fifth' src={planeSvg} alt='.'/>
+            <p className='logo-text-fifth'>FakePage<br></br><span className='digital-text-fifth'>Digital</span></p>
+          </div>
+          <p className='comece-hoje'>comece hoje mesmo</p>
+          <p className='desconto'>De <em className='off'>R$10.000</em> por</p>
+          <p>12x de <em className='preco-parcelado'>R$208,33</em></p>
+          <p>Ou R$ 2,000,00 à vista (20% de desconto)</p>
+          <button>Quero me inscrever</button>
+          <p>Veja o que você recebe se INSCREVENDO AGORA</p>
+          <ul>
+            <li>Treinamento completo Nômade Digital com 7 módulos de conteúdo</li>
+            <li>Bônus Introdução Face Ads com Paulo Berto - R$997</li>
+            <li>Bônus Copywriting com Luiz Drize - R$500</li>
+            <li>Bônus SEO com equipe Kobemedia - R$300</li>
+            <li>Bônus Como Bombar no Tik Tok Com Mustache - R$700</li>
+          </ul>
+          <button>Inscreva-seagora</button>
         </div>
       </div>
     </div>
