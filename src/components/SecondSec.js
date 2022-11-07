0679582fdd8f815e33f60b94c11f0e1380b2552a
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-07 17:00:46 -0300

    removed dynamic cards

--- src/components/SecondSec.js
+++ src/components/SecondSec.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import PlacesCards from './PlacesCards'
 import './SecondSec.css'
 import DynamicCards from './DynamicCards';
-
-
+import PublicIcon from '@mui/icons-material/Public';
+import PaidIcon from '@mui/icons-material/Paid';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const SecondSec = () => {
   return(
@@ -19,6 +20,12 @@ const SecondSec = () => {
       </div>
       <div className='dinamic-part'>
         <DynamicCards/>
+      </div>
+      <div className='tres-liberdades'>
+        <p className='tl-titulo'>Três liberdades!</p>
+        <p className='tl-texto'><PublicIcon className='icon-second'/> Liberdade Geográfica</p>
+        <p className='tl-texto'><PaidIcon className='icon-second'/> Liberdade Financeira</p>
+        <p className='tl-texto'><AccessTimeFilledIcon className='icon-second'/> Liberdade de Tempo</p>
       </div>
     </div> 
   )
