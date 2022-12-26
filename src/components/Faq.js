aa7a79e270dc4db58adad386653726a2d3bb1c00
Renan Araújo <renan.araujoonline@gmail.com>
2022-12-26 12:14:04 -0300

    fixed accordion text size

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -55,30 +55,30 @@ export default function CustomizedAccordions() {
     <div>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography sx={{fontSize: '2rem'}}>O treinamento Nômade Digital funciona para qualquer pessoa?</Typography>
+          <Typography sx={{fontSize: '1.5rem'}}><strong>O treinamento Nômade Digital funciona para qualquer pessoa?</strong></Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize: '1.5rem'}}>
+          <Typography sx={{fontSize: '1.3rem'}}>
           SIM! O curso Nômade Digital funciona para qualquer pessoa, você só precisa se dedicar e colocar em prática todos os ensinamentos dele
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography sx={{fontSize: '2rem'}}>Preciso viajar para fazer parte do treinamento?</Typography>
+          <Typography sx={{fontSize: '1.5rem' }}><strong>Preciso viajar para fazer parte do treinamento?</strong></Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize: '1.5rem'}}>
+          <Typography sx={{fontSize: '1.3rem'}}>
           NÃO! Viajar é totalmente opcional. 80% do treinamento é sobre geração de $$$ pela internet
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography sx={{fontSize: '2rem'}}>E se eu não tiver tempo para fazer o treinamento?</Typography>
+          <Typography sx={{fontSize: '1.5rem'}}><strong>E se eu não tiver tempo para fazer o treinamento?</strong></Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography sx={{fontSize: '1.5rem'}}>
+          <Typography sx={{fontSize: '1.3rem'}}>
           As aulas são 100% online e você pode fazer o treinamento no seu próprio ritmo e dentro das suas possibilidades e você terá acesso à área de membros durante 2 anos.
           </Typography>
         </AccordionDetails>
